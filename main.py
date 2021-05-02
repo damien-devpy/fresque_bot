a4be153a499da9a869719a17de986f0055a33ed2
Damien <61849659+damien-devpy@users.noreply.github.com>
2021-05-02 13:53:27 +0200

    Sentry (#7)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sentry_sdk
+
 from bot.bot import Bot
 
 
@@ -7,4 +9,12 @@ def main():
 
 
 if __name__ == "__main__":
+    sentry_sdk.init(
+        "https://1e0df56b7d12486fb0fea9fe2a47cb0f@o472074.ingest.sentry.io/5744827",
+    
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0
+    )
     main()

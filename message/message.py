a52f8e8b0fb6f7c01eefe0c7e7006eb5305514e4
Damien <61849659+damien-devpy@users.noreply.github.com>
2021-05-14 18:13:03 +0200

    Back to the line in tweet message (#9)

--- message/message.py
+++ message/message.py
@@ -1,7 +1,7 @@
 class Message:
     """Compute pre registered message with inputs"""
 
-    MESSAGE = "{counter}.\n\nC'est le nombre de personnes sensibilisées par @FresqueDuClimat entre 2018 et aujourd'hui!\n\nFaites grimper le compteur !\n\nInscrivez-vous à une Fresque du Climat 👇 #FresqueDuClimat #ClimateCollage #GIEC #ClimateEducation https://tinyurl.com/2zp9938a"
+    MESSAGE = "{counter}.\n\nC'est le nombre de personnes sensibilisées par @FresqueDuClimat entre 2018 et aujourd'hui!\n\nFaites grimper le compteur !\n\nInscrivez-vous à une Fresque du Climat 👇\n#FresqueDuClimat #ClimateCollage #GIEC #ClimateEducation https://tinyurl.com/2zp9938a"
 
     def __init__(self, counter):
 
--- tests/unit/test_message.py
+++ tests/unit/test_message.py
@@ -17,5 +17,5 @@ def test_that_message_compute_with_input():
 
     assert (
         message.message
-        == "42.\n\nC'est le nombre de personnes sensibilisées par @FresqueDuClimat entre 2018 et aujourd'hui!\n\nFaites grimper le compteur !\n\nInscrivez-vous à une Fresque du Climat 👇 #FresqueDuClimat #ClimateCollage #GIEC #ClimateEducation https://tinyurl.com/2zp9938a"  # noqa: W503
+        == "42.\n\nC'est le nombre de personnes sensibilisées par @FresqueDuClimat entre 2018 et aujourd'hui!\n\nFaites grimper le compteur !\n\nInscrivez-vous à une Fresque du Climat 👇\n#FresqueDuClimat #ClimateCollage #GIEC #ClimateEducation https://tinyurl.com/2zp9938a"  # noqa: W503
     )

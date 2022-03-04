64d5df8d9db2c5d4ed9139a351e1e6d4703ec5a4
Damien <61849659+damien-devpy@users.noreply.github.com>
2022-03-04 20:20:12 +0100

    Linting (#11)
    * Linting
    
    * Update the readme

--- tests/unit/test_counter.py
+++ tests/unit/test_counter.py
@@ -1,15 +1,14 @@
 import pytest
 
 from counter.counter import Counter
-from counter.missingcounterexception import MissingCounterException
 
 
 def test_that_counter_successfully_get_counter(content):
-    counter = Counter(content)
-    assert counter.counter == 327342
+    assert Counter(content).counter == 327342
+
 
 def test_that_counter_successfully_raise_exception_when_counter_cant_be_found(
     wrong_content,
 ):
-    with pytest.raises(KeyError) as err:
-        counter = Counter(wrong_content)
+    with pytest.raises(KeyError):
+        Counter(wrong_content)
